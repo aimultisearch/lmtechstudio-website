b96a27ccc17954d44eeeab3b4709bc9a792c4aaf
LM Tech Studio <lmtechstudio.contact@gmail.com>
2026-05-25 22:04:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // ── Modal PRO Features ────────────────────────────────────────────────────────
-
 function openModal() {
   const overlay = document.getElementById('modalOverlay');
   overlay.classList.add('active');
@@ -12,7 +11,23 @@ function closeModal() {
   document.body.style.overflow = '';
 }
 
+// ── Modal Launch ──────────────────────────────────────────────────────────────
+function openModalLaunch() {
+  const overlay = document.getElementById('modalOverlay2');
+  overlay.classList.add('active');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeModalLaunch() {
+  const overlay = document.getElementById('modalOverlay2');
+  overlay.classList.remove('active');
+  document.body.style.overflow = '';
+}
+
 // Fermer avec la touche Escape
 document.addEventListener('keydown', function(e) {
-  if (e.key === 'Escape') closeModal();
+  if (e.key === 'Escape') {
+    closeModal();
+    closeModalLaunch();
+  }
 });
